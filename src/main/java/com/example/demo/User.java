5d400a3c0465413a89153dd7abb193b5ccaae3de
Sergey <sergey.rymar.ne@yandex.ru>
2018-11-24 00:48:49 +0300

    add class User,with anotetions and spring-boot-starter-jpa

--- src/main/java/com/example/demo/User.java
+++ src/main/java/com/example/demo/User.java
@@ -3,5 +3,23 @@ package com.example.demo;/*
  *autor Sergey on 23.11.2018 17:30
  */
 
+import lombok.Getter;
+import lombok.Setter;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
 public class User {
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String name;
+
+
 }
